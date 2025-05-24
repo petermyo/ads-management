7bea5560563d50b227f870c27d4407e461f4e025
petermyo <p3t3rmyo@gmail.com>
2025-05-24 13:05:28 +0630

    Update index.js

--- functions/api/index.js
+++ functions/api/index.js
@@ -2,11 +2,6 @@
 export async function onRequest({ request }) {
   const url = new URL(request.url);
 
-  if (url.pathname === '/api/users') {
-    return new Response(JSON.stringify({ users: ['Alice', 'Bob'] }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
 
-  return new Response('Not Found', { status: 404 });
+  return new Response('API Page', { status: 404 });
 }
